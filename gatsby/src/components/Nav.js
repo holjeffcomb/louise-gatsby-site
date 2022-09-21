763a1ba0e52c8e03c913e2fea71f394c9f0bba48
holjeffcomb <72577303+holjeffcomb@users.noreply.github.com>
2022-09-21 11:44:58 -0700

    initial commit

---
+++ gatsby/src/components/Nav.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import { Link } from 'gatsby';
+import styled from 'styled-components';
+import Logo from './Logo';
+
+const NavStyles = styled.nav`
+  margin-bottom: 3rem;
+  ul {
+    margin: 0;
+    padding: 0;
+    display: grid;
+    grid-template-columns: 1fr 1fr auto 1fr 1fr;
+    grid-gap: 2rem;
+    text-align: center;
+    list-style: none;
+    display: grid;
+    grid-template-columns: 1fr 1fr auto 1fr 1fr;
+    grid-gap: 2rem;
+    align-items: center;
+    margin-top: -6rem;
+  }
+  li {
+    --rotate: -2deg;
+    transform: rotate(var(--rotate));
+    order: 1;
+    &:nth-child(1) {
+      --rotate: 1deg;
+    }
+    &:nth-child(2) {
+      --rotate: -2.5deg;
+    }
+    &:nth-child(4) {
+      --rotate: 2.5deg;
+    }
+    &:hover {
+      --rotate: 3deg;
+    }
+  }
+  a {
+    font-size: 3rem;
+    text-decoration: none;
+    &:hover {
+      color: var(--red);
+    }
+    /*
+      If the link matches the current page, add highlight
+    */
+    &[aria-current='page'] {
+      color: var(--red);
+    }
+  }
+`;
+
+export default function Nav() {
+  return (
+    <NavStyles>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/">
+            <Logo />
+          </Link>
+        </li>
+        <li>
+          <Link to="/offerings">Offerings</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+    </NavStyles>
+  );
+}
---
+++ gatsby/src/pages/404.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const NotFoundPage = () => <p>Oops couldn't find this page</p>;
+
+export default NotFoundPage;
+
+export const Head = () => <title>Not found</title>;
